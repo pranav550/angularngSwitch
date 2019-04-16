be90c7281469d27043c60a331b9dae29de077e59
pranav <pranavverma550@gmail.com>
2019-04-16 07:10:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularngSwitch';
+  public choose=""
+  setvalue(drp:any){
+    this.choose=drp.target.value
+  }
 }
